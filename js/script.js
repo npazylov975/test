f45ccec9409a8abad5695581d690e7b2ae9ee680
npazylov975 <nurzamanpazylov975@gmail.com>
2022-10-15 14:07:05 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
